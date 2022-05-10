e43887c2ab5a0647d5d1a4fd925870705f426b86
Ion Comanici <ioncomanici@gmail.com>
2022-05-10 18:32:09 +0100

    pus culori pe triunghi

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
-// use web_sys::console;
+use rand::prelude::*;
+use web_sys::console;
 use wasm_bindgen::JsCast;
+
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -20,12 +22,26 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
         draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+
+        draw_triangle(&context, [(300.0, 0.0), (150.0, 300.0), (450.0, 300.0)]);
     Ok(())
 }
 
 
-fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3]
+) {
+    let mut rng = thread_rng();
+    let color:(i32, i32, i32) = (rng.gen_range(0..255),rng.gen_range(0..255),rng.gen_range(0..255),);
+
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+
+    console::log_1(&JsValue::from_str(&color_str));
+
         let [top, left, right] = points;
+
         context.move_to(top.0, top.1);
         context.begin_path();
         context.line_to(left.0, left.1);
@@ -33,4 +49,6 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64
         context.line_to(top.0, top.1);
         context.close_path();
         context.stroke();
+        context.fill();
+
 }
