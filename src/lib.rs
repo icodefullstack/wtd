ea471b3d2586fcb2c98dd6542646804c077b9fdd
Ion Comanici <ioncomanici@gmail.com>
2022-05-11 18:33:00 +0100

    end day 11-5-22

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 use rand::prelude::*;
-use web_sys::console;
+use web_sys::{console, HtmlImageElement};
 use wasm_bindgen::JsCast;
 
 // This is like the `main` function, except for JavaScript.
@@ -9,7 +9,6 @@ pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-    let mut depth = 3;
     let canvas = document
         .get_element_by_id("canvas")
         .unwrap()
@@ -21,62 +20,9 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-
-        
-        sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], depth);
+    let img = web_sys::HtmlImageElement::new().unwrap();
+    img.set_src("Idle (1).png");
+    context.draw_image_with_html_image_element(&img, 0.0, 0.0);
 
     Ok(())
-}
-
-fn midpoint(p1:(f64, f64),p2:(f64,f64))->(f64, f64){
-    ((p1.0 + p2.0)/2.0, (p1.1 + p2.1)/2.0)
-}
-
-fn sierpinski(context:&web_sys::CanvasRenderingContext2d,
-     points:[(f64,f64); 3], depth:i8){
-
-        let points_str = format!("{:?}", points);
-        console::log_1(&JsValue::from_str(&points_str));
-
-        draw_triangle(&context, [points[0], points[1], points[2]]);
-
-        let depth_str = format!("za depth is {}", depth);
-         if depth > 0{
-            console::log_1(&JsValue::from_str(&depth_str));
-            let depth = depth-1;
-            let pt = midpoint(points[0], points[1]);
-            let pl = midpoint(points[0], points[2]);
-            let pr = midpoint(points[1], points[2]);
-            
-            sierpinski(&context, [pt, points[1], pr],depth);
-            sierpinski(&context, [pr, pl, points[2]],depth);
-            sierpinski(&context, [points[0], pt, pl],depth);
-         }
-
-}
-
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3]
-) {
-    let mut rng = thread_rng();
-    let color:(i32, i32, i32) = (rng.gen_range(0..255),rng.gen_range(0..255),rng.gen_range(0..255),);
-
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-    console::log_1(&JsValue::from_str(&color_str));
-
-        let [top, left, right] = points;
-
-        context.move_to(top.0, top.1);
-        context.begin_path();
-        context.line_to(left.0, left.1);
-        context.line_to(right.0, right.1);
-        context.line_to(top.0, top.1);
-        context.close_path();
-        context.stroke();
-        context.fill();
-
 }
