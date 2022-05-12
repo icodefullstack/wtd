dd9b207881d1b079ef5e0b3147015cc1be4e59bc
Ion Comanici <ioncomanici@gmail.com>
2022-05-12 18:27:22 +0100

    Loade first image

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use wasm_bindgen::prelude::*;
 use rand::prelude::*;
 use web_sys::{console, HtmlImageElement};
 use wasm_bindgen::JsCast;
+use std::sync::Mutex;
+use std::rc::Rc;
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -20,9 +22,39 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-    let img = web_sys::HtmlImageElement::new().unwrap();
-    img.set_src("Idle (1).png");
-    context.draw_image_with_html_image_element(&img, 0.0, 0.0);
+
+
+
+    wasm_bindgen_futures::spawn_local(async move{
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let (error_tx, error_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let img = web_sys::HtmlImageElement::new().unwrap();
+        img.set_src("Idle (1).png");
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok()
+            .and_then(|mut opt| opt.take()) {
+            success_tx.send(Ok(()));
+        }});
+
+        let error_cb  = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok()
+            .and_then(|mut opt| opt.take()) {
+            error_tx.send(Err(err));
+            }    
+        });
+
+
+        img.set_onload(Some(callback.as_ref().unchecked_ref()));
+        img.set_onerror(Some(error_cb.as_ref().unchecked_ref()));
+        success_rx.await;
+        context.draw_image_with_html_image_element(&img, 0.0, 0.0);
+        
+    });
 
     Ok(())
 }
